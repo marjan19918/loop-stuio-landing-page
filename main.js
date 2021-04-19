5c5ac35b8c038a35a8b21a9ff1870f9b30484313
marjan19918 <msajedishakker@gmail.com>
2021-04-19 00:36:45 -0700

    the first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const hambut=document.getElementById('hambut')
+const linktab=document.getElementById('link')
+const close =document.getElementById('close')
+function toogleham(){
+    
+    linktab.style.display='block'
+   
+}
+function closeham(){
+    linktab.style.display='none'
+}
